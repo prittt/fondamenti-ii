30dae9623dfa9a97bcd94abc544cbfe12a629c8f
Federico Bolelli <federico.bolelli@unimore.it>
2020-05-11 18:36:57 +0200

    Add missing files

---
+++ list/int_vector/list_int_vector.h
@@ -0,0 +1,16 @@
+
+
+//ElemType ElemCopy(const ElemType *e)
+//{
+//    return *e;
+//    //ElemType new_elem;
+//    //new_elem.data = malloc(sizeof(int)*e->size);
+//    //new_elem.size = e->size;
+//    //memcpy(new_elem.data, e->data, sizeof(int)*e->size);
+//    //return new_elem;
+//}
+//
+//void ElemDelete(ElemType *e)
+//{
+//    //free(e->data);
+//}
