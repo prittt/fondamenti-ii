e437e71e82deb5f5412e2a72aab2f40281e064a9
Federico Bolelli <federico.bolelli@unimore.it>
2021-02-11 10:07:45 +0100

    Add min-heap doc homepage

---
+++ minheap/int/homepage.h
@@ -0,0 +1,11 @@
+/**
+@mainpage Min-Heap di `int`
+
+In questa pagina trovi la documentazione per code di priorità (min-heap) di `int`. In particolare:
+
+- \ref elemtype.h contiene la definizione di `ElemType` per il tipo `int`
+e la documentazione delle funzioni a esso associate;
+- \ref minheap.h contiene la definizione del tipo `Heap` e la documentazione delle 
+funzioni primitive (e non) relative alle code di priorità (min-heap). Si noti che 
+il comportamento di queste funzioni è indipendente dalla definizione di `ElemType`.
+*/
