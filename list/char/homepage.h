adfca9d4fb8ab353f1ccf9933ee3707e9bedc2aa
Federico Bolelli <federico.bolelli@unimore.it>
2022-06-06 15:50:27 +0200

    Add char list

---
+++ list/char/homepage.h
@@ -0,0 +1,11 @@
+/**
+@mainpage Liste di `char`
+
+In questa pagina trovi la documentazione per liste di `char`. In particolare:
+
+- \ref elemtype.h contiene la definizione di `ElemType` per il tipo `char`
+e la documentazione delle funzioni a esso associate;
+- \ref list.h contiene la definizione del tipo `Item` e la documentazione delle 
+funzioni primitive (e non) relative alle liste. Si noti che il comportamento di
+queste funzioni è indipendente dalla definizione di `ElemType`.
+*/
